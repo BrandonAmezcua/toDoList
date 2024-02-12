2f7be076c2f88bbfc6b7f77bff039db1a5d3bf6d
BrandonAmezcua <brandonamezcuarex@gmail.com>
2024-02-11 21:54:55 -0800

    Implemented many changes and the code is now able to read from firebase db

--- toDoList/Views/MainView.swift
+++ toDoList/Views/MainView.swift
@@ -12,10 +12,26 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
-            // is signed in
-            ToDoListView()
+            accountView
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home",
+                          systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile",
+                          systemImage: "person.circle")
+                }
         }
     }
 }
--- toDoList/Views/ToDoListView.swift
+++ toDoList/Views/ToDoListView.swift
@@ -5,14 +5,52 @@
 //  Created by Brandon Amezcua on 1/21/24.
 //
 
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
+    @StateObject var viewModel: ToDoListViewViewModel
+    @FirestoreQuery var items: [ToDoListItem]
+    
+    init(userId: String) {
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/todos"
+        )
+        self._viewModel = StateObject(
+            wrappedValue: ToDoListViewViewModel(userId: userId)
+        )
+    }
+    
     var body: some View {
-        Text("Welcome to your account!")
+        NavigationView {
+            VStack {
+                List(items) { item in
+                    ToDoListItemView(item: item)
+                        .swipeActions {
+                            Button("Delete") {
+                                viewModel.delete(id: item.id)
+                            }
+                            .tint(.red)
+                        }
+                }
+                .listStyle(PlainListStyle())
+            }
+            .navigationTitle("To Do List")
+            .toolbar {
+                Button {
+                    // Action
+                    viewModel.showingNewItemView = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $viewModel.showingNewItemView, content: {
+                NewItemView(newItemPresented: $viewModel.showingNewItemView)
+            })
+        }
     }
 }
 
 #Preview {
-    ToDoListView()
+    ToDoListView(userId: "fdQSlxcrcJPBfamrEYR3tDKCiUy1")
 }
